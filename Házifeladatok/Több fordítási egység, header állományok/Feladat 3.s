60ddc33c7103d3ae3dd6da1ee85a40b097b88a0b
Melinda <m4gy4r.m3l1nd4@gmail.com>
2020-05-22 15:26:42 +0200

    Update Feladat 3.s

--- Házifeladatok/Több fordítási egység, header állományok/Feladat 3.s
+++ Házifeladatok/Több fordítási egység, header állományok/Feladat 3.s
@@ -4,18 +4,18 @@
  max:
  	push rbp
  	mov rbp, rsp 	
- 	sub rsp, 12		#1. paraméter (8 bájt) + 2. apraméter (4 bájt)
+ 	sub rsp, 12			#1. paraméter (8 bájt) + 2. apraméter (4 bájt)
  	mov QWORD PTR[rbp-8], rdi
  	mov DWORD PTR[rbp-12], esi
- 	mov eax, 0	#max
- 	mov ebx, 0	#i
+ 	mov eax, 0			#max
+ 	mov ebx, 0			#i
 
 COMP:
 	cmp eax, DWORD PTR[rbp-12]
 	jge OUT
  	
  	cdqe
- 	mov ecx, DWORD PTR[rdi+rax*4] #aktuális elem
+ 	mov ecx, DWORD PTR[rdi+rax*4] 	#aktuális elem
  	inc eax				#i++ 	
  	cmp ecx, ebx
  	jl COMP
