69f03f01d9c01c78cb1d58505bf96c1eb3e545ab
Melinda <m4gy4r.m3l1nd4@gmail.com>
2020-05-22 15:30:39 +0200

    Add files via upload

---
+++ Zárthelyi dolgozat/Feladat2.s
@@ -0,0 +1,23 @@
+# Magyar Melinda - Feladat 2
+
+        .intel_syntax   noprefix
+        .globl          main, alap, kitevo, a
+alap:   .4byte          3
+kitevo: .4byte          4
+a:      .4byte          1
+        .data
+
+main:
+        mov     eax, DWORD PTR[a]
+        mov     ebx, DWORD PTR[kitevo]
+        jmp     cond
+
+body:  
+        imul    eax, DWORD PTR[alap]
+        dec     ebx
+
+cond:
+        cmp     ebx, 0
+        jne     body
+
+        ret
